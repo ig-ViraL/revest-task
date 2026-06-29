4e68b08d9cf15905e089bf4780dbf949125846e2
viral-dev <viraldeshle1234@gmail.com>
2026-06-29 20:06:32 +0530

    feat(order-service): scaffold NestJS app with gRPC client config

---
+++ packages/order-service/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { OrdersModule } from './orders/orders.module';
+import { Order } from './orders/entities/order.entity';
+import { OrderItem } from './orders/entities/order-item.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'order.sqlite',
+      entities: [Order, OrderItem],
+      synchronize: true,
+    }),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'product',
+          protoPath: join(__dirname, '../../../proto/product.proto'),
+          url: 'localhost:50051',
+        },
+      },
+    ]),
+    OrdersModule,
+  ],
+})
+export class AppModule {}
