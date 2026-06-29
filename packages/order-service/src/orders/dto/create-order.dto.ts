10e3159bbdced74ed3a6cecaca413545fcf9ba02
viral-dev <viraldeshle1234@gmail.com>
2026-06-29 20:09:11 +0530

    feat(order-service): add order DTOs

---
+++ packages/order-service/src/orders/dto/create-order.dto.ts
@@ -0,0 +1,18 @@
+import { IsArray, ValidateNested, IsString, IsInt, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class OrderItemDto {
+  @IsString()
+  productId: string;
+
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}
+
+export class CreateOrderDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+}
