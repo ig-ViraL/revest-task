77fc6aa08cf2096ec8457415e231075601c0caaf
viral-dev <viraldeshle1234@gmail.com>
2026-06-29 20:09:58 +0530

    feat(order-service): add REST controller + wire module

---
+++ packages/order-service/src/orders/orders.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Patch, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly service: OrdersService) {}
+
+  @Post()
+  create(@Body() dto: CreateOrderDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Patch(':id/status')
+  updateStatus(@Param('id') id: string, @Body() dto: UpdateOrderStatusDto) {
+    return this.service.updateStatus(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  cancel(@Param('id') id: string) {
+    return this.service.cancel(id);
+  }
+}
---
+++ packages/order-service/src/orders/orders.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'product',
+          protoPath: join(__dirname, '../../../../proto/product.proto'),
+          url: 'localhost:50051',
+        },
+      },
+    ]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
